a4c469c9b3224fceaf2a950ef12f8e3939dfdac6
Joel Steres <joel@ymobility.com>
2018-10-15 13:39:27 -0700

    Use http for now.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://www.sgvai.org'
+SITEURL = 'http://www.sgvai.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
